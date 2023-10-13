f6edea455abc58cc78604a2ea323acaac31bbbe2
sahidinme <sahidinme@gmail.com>
2023-10-14 01:29:34 +0700

    Mengedit tampilan Dasbord

--- app/Http/Controllers/Back/DashboardController.php
+++ app/Http/Controllers/Back/DashboardController.php
@@ -3,12 +3,20 @@
 namespace App\Http\Controllers\Back;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('back.dashboard.index');
+        return view('back.dashboard.index', [
+            'total_articles'    => Article::count(),
+            'total_categories'  => Category::count(),
+            'latest_article'    => Article::with('Category')->whereStatus(1)->latest()->take(5)->get(),
+            'popular_article'    => Article::with('Category')->whereStatus(1)->orderBy('views', 'desc')->take(5)->get()
+
+        ]);
     }
 }
